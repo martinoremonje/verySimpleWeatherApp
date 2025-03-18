cd5d9fc55a8f99d8a8620b402e352fd7a8423849
martinoremonje <m.monjeorellana@gmail.com>
2025-03-18 15:54:00 -0300

    cambio forzado a TS para subir proyecto3

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`, // Necesario para JSX, según tu configuración
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 })
