e165811590449506f0adff489d695a89633f5d79
martinoremonje <m.monjeorellana@gmail.com>
2025-04-01 00:55:58 -0300

    cambios a index.html

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import { FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 h-28">
+    <footer className="bg-gray-800 text-white p-4 min-h-28">
       <div className="container mx-auto flex flex-col md:flex-row md:justify-between md:items-center">
         
-        <div className="text-center md:text-left mb-4 md:mb-0">
+        <div  className="text-center md:text-left mb-4 md:mb-0">
           <p>&copy; {new Date().getFullYear()} Vigesta. Todos los derechos reservados.</p>
           <hr />
           <p>Contacto: +56 9235758</p>
