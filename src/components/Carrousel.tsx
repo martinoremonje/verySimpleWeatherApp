c6624933d7246e20c6d8d9417e9182b71dca5d48
martinoremonje <m.monjeorellana@gmail.com>
2025-03-31 23:43:41 -0300

    cambios a toda la page

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,5 +1,5 @@
-import  { useState, useRef, useEffect } from 'react';
-import logo from '../assets/vigestaLogo.png'
+import { useState, useRef, useEffect } from 'react';
+import logo from '../assets/vigestaLogo.png';
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -34,14 +34,13 @@ const Carousel = ({ images }) => {
   return (
     <div id="default-carousel" className="relative w-full h-screen" data-carousel="slide">
       <div className="absolute top-4 right-4 ml-6 z-50">
-  <img data-aos="fade-up"  data-aos-duration="2000" src={logo} alt="logo" className="rounded-full w-16 h-16" />
-</div>
+        <img data-aos="fade-up" data-aos-duration="2000" src={logo} alt="logo" className="rounded-full w-16 h-16" />
+      </div>
       <div className="absolute top-4 right-0 mr-2 z-50" ref={dropdownRef}>
         <button
           onClick={toggleDropdown}
-          className="inline-flex items-center rounded-md py-2 px-4 text-white" // Fondo transparente, texto blanco
+          className="inline-flex items-center rounded-md py-2 px-4 text-white"
         >
-          
           <svg
             className={`w-4 h-4 ml-2 transform ${isDropdownOpen ? 'rotate-180' : ''}`}
             fill="none"
@@ -52,25 +51,27 @@ const Carousel = ({ images }) => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
           </svg>
         </button>
-        {isDropdownOpen && (
-          <div className="absolute right-0 mt-[2px] w-48 rounded-md shadow-lg py-1"> {/* Fondo transparente */}
-            <div className="flex flex-col items-end">
-              <a href="/contacto" className="block px-4 py-2 text-sm text-white hover:bg-gray-800/20 hover:text-gray-600"> {/* Texto blanco */}
-                Contacto
-              </a>
-              <a href="/about-us" className="block px-4 py-2 text-sm text-white hover:bg-gray-800/20 hover:text-gray-600"> {/* Texto blanco */}
-                About Us
-              </a>
-              <a href="/referencias" className="block px-4 py-2 text-sm text-white hover:bg-gray-800/20 hover:text-gray-600"> {/* Texto blanco */}
-                Referencias
-              </a>
-            </div>
+        <div
+          className={`absolute right-0 mt-[2px] w-48 rounded-md shadow-lg py-1 transition-all duration-300 ease-in-out ${
+            isDropdownOpen ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'
+          }`}
+          style={{pointerEvents: isDropdownOpen ? 'auto' : 'none'}}
+        >
+          <div className="flex flex-col items-end">
+            <a href="/contacto" className="block px-4 py-2 text-sm text-white hover:bg-gray-800/20 hover:text-gray-600">
+              Contacto
+            </a>
+            <a href="/about-us" className="block px-4 py-2 text-sm text-white hover:bg-gray-800/20 hover:text-gray-600">
+              About Us
+            </a>
+            <a href="/referencias" className="block px-4 py-2 text-sm text-white hover:bg-gray-800/20 hover:text-gray-600">
+              Referencias
+            </a>
           </div>
-        )}
+        </div>
       </div>
 
       <div className="relative h-full overflow-hidden">
-        {/* Carrusel items */}
         {images.map((image, index) => (
           <div
             key={index}
@@ -95,14 +96,12 @@ const Carousel = ({ images }) => {
         type="button"
         className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         onClick={prevSlide}
-      >
-      </button>
+      ></button>
       <button
         type="button"
         className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         onClick={nextSlide}
-      >
-      </button>
+      ></button>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/flowbite/**/*.js",
-    "./src/**/*.{html,js}"
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [require('flowbite/plugin')],
-  }
-  
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}", // Corrección importante
+    "./node_modules/flowbite/**/*.js", // Mantenemos esta línea
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
