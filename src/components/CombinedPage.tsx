9d234be6fb4b87185546dcd93dfca5d420b39ebe
martinoremonje <m.monjeorellana@gmail.com>
2025-04-02 02:03:44 -0300

    cambios a combined

--- src/components/CombinedPage.tsx
+++ src/components/CombinedPage.tsx
@@ -58,7 +58,7 @@ const CombinedPage = () => {
                 </div>
                 <div className="mb-4">
                   <label htmlFor="mensaje" className="block text-gray-700 text-sm font-bold mb-2">Mensaje:</label>
-                  <textarea id="mensaje" rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+                  
                 </div>
                 <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Enviar Mensaje</button>
               </form>
